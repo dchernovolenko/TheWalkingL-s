9b2c9e2a9cded75f9b1801c74311f201da2c4622
JackieW001 <jwoo@stuy.edu>
2017-10-28 18:44:25 -0400

    moved db connection and added sql instructions

--- root.py
+++ root.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import os
-import utils
+from utils import sqlite3lib
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
-    
+
+# database connection
+db_name = "../data/thewalkingls.db"
+db = sqlite3.connect(db_name)
+
 users = {"userNow": "userNowPass"}
 
 @app.route("/", methods = ["GET", "POST"])
@@ -60,19 +64,16 @@ def edit():
     s_title = story_info["title"]
     s_creator = story_info["owner"]
     s_story = story_info["story"]
+    # to edit run add_to_story(dbh, story), call it in read_story() and use request.args()
     return render_template("story.html", title=s_title, creator=s_creator, story=s_story, time="sometime") 
 
 @app.route("/create", methods = ["GET", "POST"])
 def create():
     user_id = request.args["user_id"]
-    # how to get form entries and put it into create_new_story(dbh)
+    # to run create_new_story(dbh, args), call it in read_story() and use request.args()
     return render_template("newstory.html", user_id)
 
 if __name__ == "__main__":
-    # database connection
-    db_name = "../data/thewalkingls.db"
-    db = sqlite3.connect(db_name)
-    
     app.debug = True
     app.run()
     
